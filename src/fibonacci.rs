42d645921650f23c258615784d19ce1002d6f822
Connor Prussin <connor@prussin.net>
2023-02-26 12:45:06 -0800

    Use Ordinal to correct ordinal string in fibonacci

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,6 +1,7 @@
+use ordinal::Ordinal;
 use std::env::args;
 
-fn fibonacci(index: u32) -> u32 {
+fn fibonacci(index: u32) -> u64 {
     let mut seq = [0, 1];
     for _ in 0..index {
         seq = [seq[1], seq[0] + seq[1]];
@@ -10,7 +11,11 @@ fn fibonacci(index: u32) -> u32 {
 
 pub fn main() {
     match args().nth(2).and_then(|s| s.parse::<u32>().ok()) {
-        Some(n) => println!("The {n}th Fibonacci number is {}", fibonacci(n)),
+        Some(n) => println!(
+            "The {} Fibonacci number is {}",
+            Ordinal(n).to_string(),
+            fibonacci(n)
+        ),
         None => println!("You must pass an index!"),
     }
 }
