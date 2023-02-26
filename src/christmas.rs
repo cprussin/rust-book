7055d911754e55be98f4f8d605f2338ec5f53214
Connor Prussin <connor@prussin.net>
2023-02-26 11:57:58 -0800

    Cleaner implementation of the christmas carol

--- src/christmas.rs
+++ src/christmas.rs
@@ -13,32 +13,41 @@ const DAYS: [(&str, &str); 12] = [
     ("twelfth", "twelve drummers drumming"),
 ];
 
-fn gifts(i: usize) -> String {
-    match i {
-        0 => DAYS[0].1.to_string(),
-        1 => format!("{} and {}", DAYS[1].1, DAYS[0].1),
-        _ => DAYS[0..i + 1]
-            .iter()
-            .enumerate()
-            .map(|(i, day)| {
-                if i == 0 {
-                    format!("and {}", day.1)
-                } else {
-                    day.1.to_string()
-                }
-            })
-            .rev()
-            .collect::<Vec<String>>()
-            .join(", "),
-    }
+fn day_ordinals() -> Vec<(usize, String)> {
+    DAYS.iter()
+        .map(|day| day.0.to_string())
+        .enumerate()
+        .collect()
+}
+
+fn gifts(day_number: usize) -> Vec<(usize, String)> {
+    DAYS[0..day_number + 1]
+        .iter()
+        .map(|day| day.1.to_string())
+        .enumerate()
+        .rev()
+        .collect()
+}
+
+fn str_when(condition: bool, string: &str) -> String {
+    (if condition { string } else { "" }).to_string()
 }
 
 pub fn main() {
-    for (i, day) in DAYS.iter().enumerate() {
-        println!(
-            "On the {} day of Christmas my true love sent to me {}",
-            day.0,
-            gifts(i)
-        );
+    println!();
+    println!();
+    println!("\t\t The Twelve Days Of Christmas");
+    println!("\t\t==============================");
+    for (day_number, day_ordinal) in day_ordinals() {
+        println!();
+        println!("On the {day_ordinal} day of Christmas my true love sent to me");
+        for (gift_number, gift) in gifts(day_number) {
+            println!(
+                "\t{}{}{}",
+                str_when(day_number > 0 && gift_number == 0, "and "),
+                gift,
+                str_when(day_number > 1 && gift_number > 0, ",")
+            );
+        }
     }
 }
