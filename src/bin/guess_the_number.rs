4b65912ad5557e44caf595e437cd2b586256b1fb
Connor Prussin <connor@prussin.net>
2023-03-02 20:53:03 -0800

    Use inquire for guess the number

--- src/bin/guess_the_number.rs
+++ src/bin/guess_the_number.rs
@@ -1,10 +1,7 @@
 use clap::Parser;
+use inquire::{validator::Validation, CustomType, CustomUserError};
 use rand::Rng;
-use std::{
-    cmp::Ordering,
-    io::{self, Write},
-    ops::RangeInclusive,
-};
+use std::{cmp::Ordering, ops::RangeInclusive};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,55 +9,44 @@ struct Cli {}
 
 const RANGE: RangeInclusive<u32> = 1..=500;
 
-fn parse_guess(guess: &str) -> Option<u32> {
-    guess
-        .parse()
-        .ok()
-        .and_then(|num| RANGE.contains(&num).then_some(num))
-}
-
-fn read_guess() -> String {
-    let mut guess = String::new();
-    print!("Please input your guess: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut guess).unwrap();
-    guess.trim().to_string()
-}
-
-fn print_invalid_guess_help(guess: &str) {
-    println!(
-        "'{}' is not a valid guess, you must guess a number between {} and {}!",
-        guess,
-        RANGE.start(),
-        RANGE.end()
-    )
-}
-
-fn get_valid_guess() -> u32 {
-    loop {
-        let guess = read_guess();
-        match parse_guess(&guess) {
-            Some(num) => return num,
-            None => print_invalid_guess_help(&guess),
-        };
-    }
-}
-
 pub fn main() {
     Cli::parse();
 
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(RANGE);
+    let secret = rand::thread_rng().gen_range(RANGE);
 
-    loop {
-        match get_valid_guess().cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    _ = CustomType::<u32>::new("Input your guess: ")
+        .with_help_message(&format!(
+            "Please enter a number between {} and {}",
+            RANGE.start(),
+            RANGE.end()
+        ))
+        .with_validator(validate_guess)
+        .with_validator(move |guess: &u32| check_guess(guess, &secret))
+        .with_formatter(&|i| format!("You win!  The number was {i}!"))
+        .prompt();
+}
+
+fn validate_guess(guess: &u32) -> Result<Validation, CustomUserError> {
+    if RANGE.contains(guess) {
+        Ok(Validation::Valid)
+    } else {
+        Ok(Validation::Invalid(
+            format!(
+                "{guess} is not between {} and {}",
+                RANGE.start(),
+                RANGE.end()
+            )
+            .into(),
+        ))
+    }
+}
+
+fn check_guess(guess: &u32, secret: &u32) -> Result<Validation, CustomUserError> {
+    match guess.cmp(secret) {
+        Ordering::Less => Ok(Validation::Invalid("Too small!".into())),
+        Ordering::Greater => Ok(Validation::Invalid("Too big!".into())),
+        Ordering::Equal => Ok(Validation::Valid),
     }
 }
