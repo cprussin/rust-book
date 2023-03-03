fbfe1e5c7ab1b33ad21b1b1090b1829f2cb9f9a9
Connor Prussin <connor@prussin.net>
2023-03-03 09:51:59 -0800

    Add median/mode calculator

---
+++ src/bin/median_mode.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+use rust_book::number_utils;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Calculate the median and mode of this list of numbers
+    #[arg(required = true)]
+    numbers: Vec<i32>,
+}
+
+fn main() {
+    let mut cli = Cli::parse();
+
+    println!("Median: {}", number_utils::median(&mut cli.numbers));
+    println!("Mode: {}", number_utils::mode(&cli.numbers));
+}
--- src/number_utils.rs
+++ src/number_utils.rs
@@ -1,7 +1,29 @@
+use std::collections::BTreeMap;
+
 pub fn fibonacci(index: u32) -> u64 {
     let mut seq = [0, 1];
     for _ in 0..index {
         seq = [seq[1], seq[0] + seq[1]];
     }
     seq[0]
+}
+
+pub fn median(values: &mut Vec<i32>) -> f64 {
+    values.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    if values.len() % 2 == 0 {
+        let half = values.len() / 2;
+        (values[half] + values[half - 1]) as f64 / 2.0
+    } else {
+        values[values.len() / 2] as f64
+    }
+}
+
+pub fn mode(values: &Vec<i32>) -> i32 {
+    let mut map = BTreeMap::new();
+    for value in values {
+        *map.entry(value).or_insert(0) += 1;
+    }
+    let mut vec = Vec::from_iter(map);
+    vec.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
+    *vec[0].0
 }
